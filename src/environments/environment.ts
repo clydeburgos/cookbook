b951eec2a62bc7d877b57a0a99ec5574e07b48c7
Clyde Burgos <clydeburgos@gmail.com>
2019-04-21 16:20:07 +0800

    update google btn and remove apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   baseUrl: 'https://localhost:44398/',
   firebase: {
-    apiKey: "AIzaSyD4fYTKvkZ7EDBWhLWc7UGHdnw4lkb98is",  
+    apiKey: "",  
     authDomain: "cookbookapp-37394.firebaseapp.com",
     databaseURL: "https://cookbookapp-37394.firebaseio.com",
     projectId: "cookbookapp-37394",
